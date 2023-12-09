759973da16be2ee6ee2adb11b5a86f263791c236
ChrisW <chrisjweiland@gmail.com>
2023-12-09 14:59:00 -0500

    bunch of leanups and added home assistant integration

--- src/garden.cpp
+++ src/garden.cpp
@@ -5,23 +5,12 @@
 //DOIT ESP32 DEVKIT V1
 //define MY_SSID, MY_WIFI_PASSWORD, RPI_ADDRESS, RPI_PORT
 #include "passwd/passwd.h"
-#include "Protocol/Protocol.h"
-#include "MQTTDevice.h"
-#include "MQTTSensor.h"
-#include "MQTT.h"
 #include <ArduinoHA.h>
 
 
-WiFiClient client;
-HADevice device;
-HAMqtt hamqtt(client, device);
-HASensorNumber SensorBattery("battery", HABaseDeviceType::PrecisionP2);
-
-WiFiClient mqttWifi;
-MQTTClient mqtt(256);
-
 //#define DEBUG
-#define ENABLE_DEBUGGING
+//#define ENABLE_DEBUGGING
+//#define ENABLE_MOISTURE_SENSING
 
 #ifdef DEBUG
 const int DeepSleepTimeUS = 10 * 1000000;
@@ -36,17 +25,19 @@ const int PumpTimeMS = 5*60 * 1000;
 const int MoistureThreshold = 2000;
 const int WaterAfterTime = 16 * 60 + 0; //hours + minutes (in minutes)
 
+Timezone myTZ;
 
-//3 to 2 yields range 0-8.25V, battery should get to max 7.3
 #define NODE_ID 1
 
 #if NODE_ID == 1
-MQTTDevice GardenDevice(mqtt, "Test Garden", "test_garden");
+#define DEVICE_NAME "Test Garden"
+#define DEVICE_ID "test_garden"
 
 const int NumSensors = 3;
 const int VoltageCalibrationTableSize = 6;
 
 //TODO: enter correct R values here - this is from node2
+//3 to 2 yields range 0-8.25V, battery should get to max 7.3
 float R1 = 255.8;//300.0;
 float R2 = 170.5;//200.0;
 float VIN = 3.3; //TODO: calibrate this!
@@ -66,7 +57,7 @@ float VoltageCalibrationTable[VoltageCalibrationTableSize][2] =
 
 
 #elif NODE_ID == 2
-MQTTDevice GardenDevice(mqtt, "Perenial Garden", "perenial_garden");
+#define DEVICE_NAME "Perenial Garden"
 
 const int NumSensors = 3;
 const int VoltageCalibrationTableSize = 6;
@@ -88,7 +79,15 @@ float VoltageCalibrationTable[VoltageCalibrationTableSize][2] =
 };
 #endif
 
-MQTTSensor BatterySensor(GardenDevice, "Battery", "battery");
+WiFiClient client;
+HADevice device;
+HAMqtt hamqtt(client, device);
+HASensorNumber BatterySensor(DEVICE_ID "_battery", HABaseDeviceType::PrecisionP2);
+HABinarySensor PumpStatus(DEVICE_ID "_pump_status");
+HABinarySensor WaterSensor(DEVICE_ID "_water_status");
+HASensor StatusSensor(DEVICE_ID "_status");
+HASensor BootTimeSensor(DEVICE_ID "_boot_time");
+HASensor LastPumpTime(DEVICE_ID "_last_pump_time");
 
 
 #define BATTERY_SENSE_PIN 36
@@ -96,11 +95,6 @@ const int SENSOR_PINS[5] = { 39, 34, 35, 32, 33 };
 #define WATER_LEVEL_PIN 23
 #define SENSOR_POWER_PIN 14
 #define PUMP_POWER_PIN 13
-
-//errors
-#define ERROR_NO_SENSORS 1
-#define ERROR_BAD_SENSOR 2
-#define ERROR_OUT_OF_WATER 3
 
 RTC_DATA_ATTR unsigned int CurrentTimeMS = 0;
 RTC_DATA_ATTR int LastWateredDay = -1;
@@ -233,46 +227,45 @@ float ReadBatteryVoltage()
   return vin;
 }
 
-void SendStartup()
+bool HaveWater()
 {
-  //TODO: implement this
+  return digitalRead(WATER_LEVEL_PIN) == LOW;
 }
 
 void SendBatteryStatus()
 {
   float vin = ReadBatteryVoltage();
-
-  BatterySensor.PublishValue(String(vin, 2));
-  SensorBattery.setValue(vin);
+  BatterySensor.setValue(vin);
 }
 
-void SendPumpEvent(unsigned char on)
+void SetPumpState(unsigned char on)
 {
-  //TODO: implement this
+  if (on)
+  {
+    digitalWrite(PUMP_POWER_PIN, HIGH);
+    LastPumpTime.setValue(myTZ.dateTime("Y/m/d H:i:s").c_str());
+  }
+  else
+  {
+      digitalWrite(PUMP_POWER_PIN, LOW);
+  }
+  PumpStatus.setState(on);
 }
 
-void SendErrorMessage(int error, const char* message)
+void SendErrorMessage(const char* message)
 {
-  //TODO: implement this
   DebugPrint("ERROR: " + String(message) + "\n");
-
-  // ErrorMessage msg;
-  // msg.m_ID = NODE_ID;
-  // strcpy(msg.m_Message, message);
-  // unsigned char buffer[300];
-  // unsigned int bufferLength = msg.Serialize(buffer, sizeof(buffer));
-
-  // if (!SocketSend(buffer, bufferLength))
-  //   return;
+  StatusSensor.setValue(message);
 }
 
-void ClearError(int error)
+void ClearError()
 {
-  //TODO: implement this (maybe not)
+  StatusSensor.setValue("Online");
 }
 
 void TakeMoistureReadings(unsigned short* readings)
 {
+#ifdef ENABLE_MOISTURE_SENSING
   //turn on the power to the sensors
   digitalWrite(SENSOR_POWER_PIN, HIGH);
   DebugPrint("Sensors are on now\n");
@@ -291,6 +284,7 @@ void TakeMoistureReadings(unsigned short* readings)
   //turn off the sensors
   digitalWrite(SENSOR_POWER_PIN, LOW);
   DebugPrint("Sensors are off\n");
+#endif
 }
 
 void SendMoistureReadings(unsigned short* readings)
@@ -301,7 +295,6 @@ void SendMoistureReadings(unsigned short* readings)
 //===========================================================================================================================
 //===========================================================================================================================
 //===========================================================================================================================
-Timezone myTZ;
 
 
 void setup() {
@@ -329,12 +322,20 @@ void setup() {
   byte mac[6];
   WiFi.macAddress(mac);
   device.setUniqueId(mac, sizeof(mac));
-  device.setName("HATest Garden");
-  //device.setSoftwareVersion("1.0.0");
+  device.setName(DEVICE_NAME);
 
-  SensorBattery.setUnitOfMeasurement("V");
-  SensorBattery.setDeviceClass("voltage");
-  BatterySensor.Init("voltage", "V");
+  Serial.println("Starting ha integration...");
+  bool result = hamqtt.begin("192.168.1.98", MQTT_USER, MQTT_PASSWORD);
+  Serial.printf("begin = %c\n", result ? 'Y' : 'N');
+
+  BatterySensor.setUnitOfMeasurement("V");
+  BatterySensor.setDeviceClass("voltage");
+  StatusSensor.setName("Status");
+  BootTimeSensor.setName("Boot Time");
+  LastPumpTime.setName("Last Watered");
+  PumpStatus.setName("Pump Status");
+  WaterSensor.setName("Water Level");
+  hamqtt.loop(); //apparently it helps to call this once BEFORE you start publishing (boot time was not publishing)
 
   waitForSync();
   myTZ.setLocation("America/New_York");
@@ -343,10 +344,30 @@ void setup() {
   DebugPrint("Wakeup cause = " + String(wakeup_reason) + "\n");
 
   if (wakeup_reason < 1 || wakeup_reason > 5)
-    SendStartup();
-  SendBatteryStatus();
+  {
+    //this is a cold start (not waking from deep sleep)
+    DebugPrint("Boot time = " + myTZ.dateTime("Y/m/d H:i:s") + "\n");
+    BootTimeSensor.setValue(myTZ.dateTime("Y/m/d H:i:s").c_str());
+  }
   DebugPrint("Setup actually complete\n");
   DebugPrint("Boot time = " + String(bootTime) + "; now = " + String(GetTimeMS()) + "\n");
+
+  //update our status
+  SendBatteryStatus();
+  bool haveError = false;
+  bool haveWater = HaveWater();
+  WaterSensor.setState(haveWater);
+  if (!HaveWater())
+  {
+    SendErrorMessage("Water is low");
+    haveError = true;
+  }
+  PumpStatus.setState(false);
+
+  if (!haveError)
+  {
+    ClearError();
+  }
 }
 
 int ComputeAverageMoisture(unsigned short* readings)
@@ -378,24 +399,14 @@ int ComputeAverageMoisture(unsigned short* readings)
   if (validReadings == 0)
   {
     DebugPrint("All sensors are bad!\n");
-    SendErrorMessage(ERROR_NO_SENSORS, "All sensors are bad");
+    SendErrorMessage("All sensors are bad");
     return 0x7FFFFFFF;
   }
 
-  //at least 1 sensor is working, so reset the ERROR_NO_SENSORS last error time
-  ClearError(ERROR_NO_SENSORS);
-  
   if (badSensor != -1)
   {
-    char messageBuffer[256] = {0};
     String message = "Sensor " + String(badSensor) + " is malfunctioning";
-    message.toCharArray(messageBuffer, sizeof(messageBuffer));
-    SendErrorMessage(ERROR_BAD_SENSOR, messageBuffer);
-  }
-  else
-  {
-    //all of our sensors are working - clear ERROR_BAD_SENSOR error
-    ClearError(ERROR_BAD_SENSOR);
+    SendErrorMessage(message.c_str());
   }
 
   int averageValue = sum / validReadings;
@@ -405,6 +416,7 @@ int ComputeAverageMoisture(unsigned short* readings)
 
 void DoTheThings()
 {
+#ifdef ENABLE_MOISTURE_SENSING
   //take moisture readings
   unsigned short readings[MoistureReadings::MaxReadings] = {0};
   TakeMoistureReadings(readings);
@@ -416,6 +428,7 @@ void DoTheThings()
     DebugPrint("Garden bed is not dry enough, going to sleep\n");
     return;
   }
+#endif
 
   int hour = myTZ.hour();
   int minute = myTZ.minute();
@@ -435,42 +448,41 @@ void DoTheThings()
   }
 
   //make sure there is enough water to pump
-  if (digitalRead(WATER_LEVEL_PIN) == HIGH)
+  if (!HaveWater())
   {
     //not enough water!
     DebugPrint("Want to water, but the water is empty\n");
-    SendErrorMessage(ERROR_OUT_OF_WATER, "Out of water");
+    SendErrorMessage("Water is low");
     return;
   }
 
-  //we have enough water, so clear the ERROR_OUT_OF_WATER error
-  ClearError(ERROR_OUT_OF_WATER);
-  
   //turn on the pump for a bit!
   //send message to hub saying pump got turned on
-  SendPumpEvent(1);
   DebugPrint("Turning on pump\n");
+  SetPumpState(1);
   LastWateredDay = myTZ.dayOfYear();
-  digitalWrite(PUMP_POWER_PIN, HIGH);
   for (int i = 0; i * 1000 < PumpTimeMS; i++)
   {
     //check to see if there is still water left
     if (digitalRead(WATER_LEVEL_PIN) == HIGH)
     {
       //out of water!!
-      SendErrorMessage(ERROR_OUT_OF_WATER, "Out of water while pumping");
+      SendErrorMessage("Water is low");
       break;
     }
+    //periodically send the battery status so we can see it draining while pumping
+    if ((i % 30) == 0)
+      SendBatteryStatus();
     delay(1000);
   }
   DebugPrint("Turning off pump\n");
-  digitalWrite(PUMP_POWER_PIN, LOW);
-  //send message to hub saying pump got turned off
-  SendPumpEvent(0);
+  SetPumpState(0);
 
   //update the moisture after the pumping is done
+#ifdef ENABLE_MOISTURE_SENSING
   TakeMoistureReadings(readings);
   SendMoistureReadings(readings);
+#endif
   //send the battery status too since we probably drained it pretty good
   SendBatteryStatus();
 }
@@ -479,18 +491,16 @@ void loop()
 {
   DebugPrint("Entered loop\n");
   events(); //ezTime events()
-  mqtt.loop();
+
+  hamqtt.loop();
+  DoTheThings();
   hamqtt.loop();
 
-  DoTheThings();
-
-  mqtt.loop();
   DebugPrint("going to sleep\n");
   delay(50);
   //update our time tracker
   CurrentTimeMS += millis() - bootTime + DeepSleepTimeUS / 1000;
   //go to sleep
-  mqtt.disconnect();
   esp_sleep_enable_timer_wakeup(DeepSleepTimeUS);
   esp_deep_sleep_start();
 }
